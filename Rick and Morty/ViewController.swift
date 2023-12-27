f189b1f0b7de51693451f71dee042c8a1767166b
Vitalii Iakushev <vsiakushev@gmail.com>
2023-12-27 10:26:10 -0800

    Initial Commit

---
+++ Rick and Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty
+//
+//  Created by Vitaliy Iakushev on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
